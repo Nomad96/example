4f9de2856f2108ddb8b4046a5b16e618d5ac219d
Nomad <bats.96@mail.ru>
2017-08-20 15:43:34 +0400

    01

---
+++ kino/js/ready.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+            setTimeout(function(){
+           $('body').addClass('loaded');
+                },1000);//timeout
+//            setTimeout(function(){
+//                $('.pl').css('display','none')
+//            },1700);//timeout
+        }); //ready
