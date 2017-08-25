d8eba8a5075fd363a4fdfb0713c045ca1ca5eb08
Nomad <bats.96@mail.ru>
2017-08-25 18:11:29 +0400

    18:11 08.25

--- Mobi/js/script.js
+++ Mobi/js/script.js
@@ -3,4 +3,20 @@ $(document).ready(function () {
                 $('.abs').fadeToggle('slow');
                 return false;
             }); //click
+    $(window).scroll(function(){
+        if($(this).scrollTop()>400){
+            $('.top').fadeIn();
+        }
+        else{
+            $('.top').fadeOut();
+        }
+    });//scroll
+    $(".top a").click(function(){
+        var a=$(this).attr('href');
+        var b=$(a).offset().top;
+        $('body').animate({
+            scrollTop:b
+        },1000);//animation
+        return false;
+    });//click
         }); //ready
